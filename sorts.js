f767432fe20a224b01fcd9689517cc2ec974e334
landonholmes <landon.holmes@gmail.com>
2014-10-25 19:11:48 -0500

    adding bubble and quick sort

--- sorts.js
+++ sorts.js
@@ -1,8 +1,8 @@
 /*this file is for implementing other sorts for comparison against radix sort*/
 
-function insertionSort(arr) {
-
-    var value;     // the value currently being compared
+function insertionSort(arr)
+{
+    var value; /*variable to hold current compare value*/
 
     for (var i=0; i < arr.length; i++) {
         /*store the current value because it may shift later*/
@@ -20,8 +20,8 @@ function insertionSort(arr) {
 }
 
 /*mergeSort function*/
-function mergeSort(arr){
-
+function mergeSort(arr)
+{
     if (arr.length < 2) {
         return arr;
     }
@@ -31,14 +31,14 @@ function mergeSort(arr){
         right   = arr.slice(middle),
         params = merge(mergeSort(left), mergeSort(right));
 
-    // Add the arguments to replace everything between 0 and last item in the array
     params.unshift(0, arr.length);
     arr.splice.apply(arr, params);
     return arr;
 }
 
 /*mergeSort helper function*/
-function merge(left, right){
+function merge(left, right)
+{
     var result  = [],
         leftIndex = 0,
         rightIndex = 0;
@@ -50,6 +50,83 @@ function merge(left, right){
             result.push(right[rightIndex++]);
         }
     }
-
     return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+/*bubbleSort*/
+function bubbleSort(arr)
+{
+    var len = arr.length,
+        i,
+        j,
+        stop;
+
+    for (i=0; i < len; i++){
+        for (j=0, stop=len-i; j < stop; j++){
+            if (arr[j] > arr[j+1]){
+                var temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+/*quickSort*/
+function quickSort(arr, left, right)
+{
+    var index;
+
+    /*make sure array has length*/
+    if (arr.length > 1) {
+        left = typeof left != "number" ? 0 : left;
+        right = typeof right != "number" ? arr.length - 1 : right;
+
+        /*split the array*/
+        index = partition(arr, left, right);
+
+        /*check then sort left*/
+        if (left < index - 1) {
+            quickSort(arr, left, index - 1);
+        }
+
+        /*check then sort right*/
+        if (index < right) {
+            quickSort(arr, index, right);
+        }
+    }
+    return arr;
+}
+
+/*helper partition function for quicksort*/
+function partition(arr, left, right)
+{
+    var pivot = arr[Math.floor((right + left) / 2)],  // pivot value is middle item
+        i = left,
+        j = right;
+
+    /*loop while indices aren't the same*/
+    while (i <= j) {
+        while (arr[i] < pivot) {
+            i++;
+        }
+
+        // if right item is greater, continue left
+        while (arr[j] > pivot) {
+            j--;
+        }
+
+        if (i <= j) {
+            var temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+
+            i++;
+            j--;
+        }
+    }
+
+    return i;
 }
